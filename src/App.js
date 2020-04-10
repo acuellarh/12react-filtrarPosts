5f70d03740305929edd7342bfbe7d37433faf4b2
Alejandro <acuellarh@gmail.com>
2020-04-10 18:32:42 -0500

    basic search text feature addedd

--- src/App.js
+++ src/App.js
@@ -5,10 +5,25 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {  
+
+  constructor(props){
+    super(props);
+    this.state = {
+      input: ''
+    }
+  }
+
+  updateInput(event){
+    this.setState ({
+      input: event.target.value,
+    })
+  }
   
   render() {
 
+    const inputlow = this.state.input.toLowerCase()
     const list = posts
+      .filter(post => post.title.toLowerCase().includes(inputlow))
       .map((post, index) =>   
         <li key={index}>
           <a href={post.url}><img src={post.image } alt={'image of '+post.title}/></a>
@@ -18,7 +33,7 @@ class App extends Component {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" value={this.state.input} onChange={this.updateInput.bind(this)} placeholder="Ingresa el término de búsqueda" />
         </div>
         <ul>
         {list}
